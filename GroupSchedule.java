f35a242f2f4085cba5cfa3e3472d76d8cc04c696
lekhoa1001 <35730958+lekhoa1001@users.noreply.github.com>
2018-11-30 17:25:13 -0600

    Add files via upload

---
+++ GroupSchedule.java
@@ -0,0 +1,56 @@
+
+public class GroupSchedule {
+    char members,greyscale,schedule,time;
+
+    public char massGetSchedule() {
+        //create master event array (group events)
+        //fetch member list
+        //store list of member in array
+        //iterate through member list
+        //infunction fetch schedule and in second array store iteration number
+        //infuction post to group event
+        //store in group schedule
+        return schedule;
+    }
+    public char formatGroupSchedule(schedule s,boolean weekly ){
+        //check conditional
+        //use format to print on screen
+        return schedule;
+    }
+
+    public char assertTimeBlocks(schedule s, int color[]){
+        //check for overlaps
+        //assert visual blocks
+        //color blocks
+        return time;
+    }
+    public char colorAndGrayBlocks(events e, int color){
+
+        //color in time block with solor from array
+
+        return greyscale;
+    }
+
+    public char fillBlockPopUps(){
+        //on click, fetch event from group schedule
+        //fetch data
+        //display in window
+        return 0;
+    }
+}
+
+    class group extends GroupSchedule{
+        char members, groupSchedule;
+
+       public char getGroupSchedule(){
+        return members;
+        }
+
+        class GroupPage extends GroupSchedule{
+            char accordionMenu, groups;
+
+            public char getUserGroups() {
+            return groups;
+            }
+        }
+        }
