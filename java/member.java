ac9f29b93d9a2ab3875ad9540c654c5357b11da7
lekhoa1001 <35730958+lekhoa1001@users.noreply.github.com>
2018-11-30 17:55:35 -0600

    Delete member.java

--- java/member.java
+++
@@ -1,8 +0,0 @@
-public class member {
-    char name,account,group,events;
-
-    public char getUserSchedule(){return events;}
-    public char getUserGroups(){return group;}
-    public char formatScheduleView(){return events;} //same as group but for single user
-
-}
